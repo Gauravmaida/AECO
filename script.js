7f48e43a70b1071450975db3c5b1edcf69cea535
“Gaurav <gauravmaida0121@gmail.com>
2024-04-15 08:42:08 +0530

    AecoPage

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    function showSlide(n) {
+        slides[currentSlide].classList.remove("active");
+        currentSlide = (n + slides.length) % slides.length;
+        slides[currentSlide].classList.add("active");
+    }
+
+    function nextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    setInterval(nextSlide, 3000); // Change slide every 3 seconds
+});
